89eb6d57c491611704438a6312d643ba22727788
Sharma <S4408B@corp1.ad1.seb.net>
2018-06-11 14:14:30 +0200

    adding changelogo.js

--- js/changelogo.js
+++ js/changelogo.js
@@ -1,6 +1,7 @@
+"use strict";
+
 #header-logo {
   background-image: url("http://www.fnordware.com/superpng/pnggrad8rgb.png");
   width: 190px;
   height: 20px;
-  Content-Type: application/javascript
 }
