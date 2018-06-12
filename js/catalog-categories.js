c97178996929ccbd55d5672dcc2e40d27969596e
Sharma <S4408B@corp1.ad1.seb.net>
2018-06-12 11:54:52 +0200

    adding css file

---
+++ js/catalog-categories.js
@@ -0,0 +1,37 @@
+// Add a Featured category section at the top of the catalog
+window.OPENSHIFT_CONSTANTS.CATALOG_CATEGORIES.unshift({
+  // Required.  Must be unique
+  id: "SEBShift",
+  // Required
+  label: "SEBShift",
+  // Optional.  If specified, each item in the category will utilize this icon
+  // as a default
+  iconClassDefault: "fa fa-code",
+  items: [
+    {
+      // Required.  Must be unique
+      id: "jhp-liberty",
+      // Required
+      label: "jhp-liberty",
+      // Optional.  If specified, defines a unique icon for this item
+      iconClass: "font-icon icon-go-gopher",
+      // Optional.  If specified, enables matching other tag values to this
+      // category item
+      categoryAliases: [
+        "jhp-liberty"
+      ],
+      // Optional.  If specified, will display below the item label
+      description: "This is a jhp liberty image provided by openshift."
+    },
+    {
+      // Required.  Must be unique
+      id: "JAAS",
+      // Required
+      label: "JAAS",
+      // Optional.  If specified, defines a unique icon for this item
+      iconClass: "font-icon icon-jenkins",
+      // Optional.  If specified, will display below the item label
+      description: "Jenkins as a service."
+    }
+  ]
+});
