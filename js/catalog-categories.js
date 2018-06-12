be190ef7a0fc2c13b6139090af823f5ff267cb4d
Sharma <S4408B@corp1.ad1.seb.net>
2018-06-12 14:25:44 +0200

    adding css file

--- js/catalog-categories.js
+++ js/catalog-categories.js
@@ -1,12 +1,12 @@
 // Find the Languages category.
 var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
-                      { id: 'category-languages' });
+                      { id: 'languages' });
 // Add Go as a new subcategory under Languages.
 
 console.log(category)
 
 
-category.subCategories.splice(2,0,{ // Insert at the third spot.
+category.items.splice(2,0,{ // Insert at the third spot.
   // Required. Must be unique.
   id: "go",
   // Required.
@@ -23,10 +23,10 @@ category.subCategories.splice(2,0,{ // Insert at the third spot.
 // Add a Featured category as the first category tab.
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   // Required. Must be unique.
-  id: "category-featured",
+  id: "featured",
   // Required
   label: "Featured",
-  subCategories: [
+  items: [
     {
       // Required. Must be unique.
       id: "go",
