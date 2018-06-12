1da901a0d91fad968b4e1b8d56db938f050f354f
Sharma <S4408B@corp1.ad1.seb.net>
2018-06-12 12:22:20 +0200

    adding css file

--- js/catalog-categories.js
+++ js/catalog-categories.js
@@ -1,3 +1,20 @@
+// Add Go to the Languages category
+var category = _.find(window.OPENSHIFT_CONSTANTS.CATALOG_CATEGORIES,
+{ id: 'languages' });
+category.items.splice(2,0,{ // Insert at the third spot
+  // Required.  Must be unique
+  id: "sebshift",
+  // Required
+  label: "sebshift",
+  // Optional.  If specified, defines a unique icon for this item
+  iconClass: "font-icon icon-go-gopher",
+  // Optional.  If specified, enables matching other tag values to this category
+  // item
+  categoryAliases: [
+    "sebshift"
+  ]
+});
+
 // Add a Featured category section at the top of the catalog
 window.OPENSHIFT_CONSTANTS.CATALOG_CATEGORIES.unshift({
   // Required.  Must be unique
